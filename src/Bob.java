f82a347e11ce1be42aebf23dd282a6c3c79dedd6
mdm911 <mdm911@yahoo.com>
2018-02-28 14:40:24 -0600

    Completed StringExercise

--- src/Bob.java
+++ src/Bob.java
@@ -2,21 +2,24 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        String answer = "";
 
-        System.out.println("Say something to Bob.");
-        String input = sc.nextLine();
+       do {
+           System.out.println("Say something to Bob.");
+           String input = sc.nextLine();
 
-        if (input.endsWith("?")){
-            System.out.println("Sure.");
-        }
-        else if(input.endsWith("!")){
-            System.out.println("Whoa, chill out!");
-        }
-        else if(input.equals("")){
-            System.out.println("Fine. Be that way!");
-        }
-        else {
-            System.out.println("Whatever.");
-        }
+           if (input.endsWith("?")) {
+               System.out.println("Sure.");
+           } else if (input.endsWith("!")) {
+               System.out.println("Whoa, chill out!");
+           } else if (input.equals("")) {
+               System.out.println("Fine. Be that way!");
+           } else {
+               System.out.println("Whatever.");
+           }
+           System.out.println("Would you like to say something else to Bob?");
+           answer = sc.nextLine().toLowerCase();
+       }
+       while(answer.startsWith("y"));
     }
 }
