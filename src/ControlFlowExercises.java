4743214dec190c6b3ad5af4976c31c4414e1d69e
mdm911 <mdm911@yahoo.com>
2018-02-27 23:12:23 -0600

    Do while displays powers of 2 less than 1000000

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i = 2;
+        long i = 2;
         do{
-            System.out.println(Math.pow(i,2));
+            System.out.println(i);
+            i *= i;
         }while (i < 1000000);
     }
 }
