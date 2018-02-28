cda39e84196d6b8f4c8cebc686b051f604ea7925
mdm911 <mdm911@yahoo.com>
2018-02-28 16:46:00 -0600

    Completed getInteger method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int input = 0;
+
         System.out.println(addition(1, 2));
         System.out.println(subtraction(5,2));
         System.out.println(multiplication(2,3));
         System.out.println(division(3,2));
         System.out.println(mod(6,2));
+
+       do {
+           System.out.println("Enter a number between 1 and 10");
+           input = sc.nextInt();
+           System.out.println(getInteger(input, 1, 10));
+       }
+       while(!getInteger(input, 1, 10));
+
     }
     public static float addition(float a, float b){
         return a + b;
@@ -33,5 +46,9 @@ public class MethodsExercises {
 
     public static float mod(float a, float b){
         return a % b;
+    }
+
+    public static boolean getInteger(int input, int min, int max){
+        return (input >= min && input <= max);
     }
 }
