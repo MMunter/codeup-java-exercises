b6ad3661353ba8a7e74f8982995baf12e6197f54
mdm911 <mdm911@yahoo.com>
2018-02-28 15:03:10 -0600

    Changed order of if/else statements

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,6 +1,13 @@
 public class StringExercise {
     public static void main(String [] args){
-        String message = "%nWe don't need no education %nWe don't need no thought control %n%nCheck \"this\" out!, \"s inside of \"s! %n%nIn windows, the main drive is usually C:\\ %n%nI can do backslashes \\, double backslashes \\\\,%nand the amazing triple backslash \\\\\\! %n" ;
+        String message = "%nWe don't need no education %nWe don't need no thought control ";
+
+        message += "%n%nCheck \"this\" out!, \"s inside of \"s!";
+
+        message += " %n%nIn windows, the main drive is usually C:\\";
+
+        message += "%n%nI can do backslashes \\, double backslashes \\\\,%nand the amazing triple backslash \\\\\\! %n" ;
+
         System.out.printf(message);
 
 
