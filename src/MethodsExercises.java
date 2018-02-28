8d89156d426251098f3327b6b9805f8cf0d2016f
mdm911 <mdm911@yahoo.com>
2018-02-28 16:00:28 -0600

    In Progress MethodsExercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,28 @@
+public class MethodsExercises {
+    public static void main(String[] args){
+        System.out.println(addition(1, 2));
+        System.out.println(subtraction(5,2));
+        System.out.println(multiplication(2,3));
+        System.out.println(division(3,2));
+        System.out.println(mod(6,2));
+    }
+    public static float addition(float a, float b){
+        return a + b;
+    }
+
+    public static float subtraction(float a, float b){
+        return a - b;
+    }
+
+    public static float multiplication(float a, float b){
+        return a * b;
+    }
+
+    public static float division(float a, float b){
+        return a / b;
+    }
+
+    public static float mod(float a, float b){
+        return a % b;
+    }
+}
