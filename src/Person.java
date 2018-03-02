31600ea625a257b6121bfe596df87b7d48fbf6eb
mdm911 <mdm911@yahoo.com>
2018-03-02 13:41:02 -0600

    Completed #2 Person.java

--- src/Person.java
+++ src/Person.java
@@ -18,12 +18,27 @@ public class Person {
     }
 
     public static void main(String[] args){
-        Person person1 = new Person ("Marcy");
+//        Person person1 = new Person ("Marcy");
+//
+//        person1.setName("Marcella");
+//        person1.sayHello();
 
-        person1.setName("Marcella");
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
-        person1.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
 
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 
 }
