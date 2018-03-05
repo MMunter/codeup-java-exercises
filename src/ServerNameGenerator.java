a6773b0011bcee96183c2d4ac7e6f36de0317d45
mdm911 <mdm911@yahoo.com>
2018-03-05 01:08:15 -0600

    Completed ServerNameGenerator, removed moviearray practice

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -7,20 +7,8 @@ public class ServerNameGenerator {
         String[] nouns = {"boy", "girl", "dog", "cat", "baby", "child", "car", "chair", "store", "friend"};
 
         randomElement(adjectives, nouns);
-
-        String[] movies = new movies [3];
-        movies[0] = {"Citizen Kane", "drama"};
-        movies[1] = {"Casablaca", "drama"};
-        movies[2] = {"Psycho", "horror"};
-
-        for (String movie : movies){
-            String drama = movie.
-            System.out.println();
-        }
-
-
-
     }
+
     public static void randomElement(String[] a, String[] b) {
            int rnd1 = new Random().nextInt(a.length);
            int rnd2 = new Random().nextInt(b.length);
